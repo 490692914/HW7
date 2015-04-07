21cad67f98c67a5f33af9c11ed8eeccd4f250f45
Shan Xiao <shan-xiao@uiowa.edu>
2015-04-07 11:13:04 -0500

    changes of admin page

---
+++ admin/manageCollection.aspx.vb
@@ -0,0 +1,5 @@
+﻿
+Partial Class admin_manageCollection
+    Inherits System.Web.UI.Page
+
+End Class
--- login.aspx.vb
+++ login.aspx.vb
@@ -8,4 +8,9 @@ Partial Class login_login
         Else : Response.Redirect("~/default.axpx")
         End If
     End Sub
+
+   
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Login1.Focus()
+    End Sub
 End Class
